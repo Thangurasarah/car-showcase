b3a39dfb139918e5fbb9663be1dfd836b4038be3
Thangurasarah <thangurasarah@gmail.com>
2024-06-13 14:52:49 +0300

    Initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Hero = () => {
+ return(
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x" >
+            <h1 className="hero_title font-extrabold text-4xl ">
+                Find, book, or rent a car --quickly
+                and easily!
+            </h1>
+            
+
+        </div>
+    </div>
+ )
+}
+export default Hero
